96016422c7d3aff22136227e128202be1462707a
VictorCali1702 <wiktor.naczk199@gmail.com>
2025-09-27 22:44:35 +0200

    NFL App Basic

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 # NFL APP in FLASK
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Hello NFL!"
+	return render_template("index.html")
+
+@app.route("/games")
+def games():
+	url = "https://www.thesportsdb.com/api/v1/json/3/eventslast.php?id=134922"
+	response = requests.get(url)
+	data = response.json()
+
+	games = data.get("results", []) # list of matches
+	return render_template("games.html", games=games)
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
-	
